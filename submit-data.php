4d6e92c811a19b9d02a757ecfca412b48d1e8ee6
Noah Piraino <penguinsnail@onebuttonmouse.com>
2018-01-06 13:30:51 -0500

    spacing

--- submit-data.php
+++ submit-data.php
@@ -34,18 +34,20 @@ $autoline = $_POST['autoline'];
 
 // Autonomous switch
 $auto-switch = $_POST['auto-switch'];
+
 // Autonomous scale
 $auto-scale = $_POST['auto-scale'];
 
 // Teleop switch
 $tele-switch = $_POST['tele-switch'];
+
 // Teleop scale
 $tele-scale = $_POST['tele-scale'];
 
 // Cubes returned
 $return-cubes = $_POST['return-cubes'];
 
-//Was levitate used
+// Was levitate used
 $levitate = $_POST['levitate'];
 
 // Parked or climbed
